f4384e9d83ca65c38babacf6439c118666e21e88
shenun102 <124623141+shenun102@users.noreply.github.com>
2024-08-11 23:34:47 +0100

    Add change color to boxes upon hover, grid resize
    - Added a feature which changes the grid
      boxes to a random color upon hover
    - Added a button which prompts the user to resize
      the grid
    - Input from user will remove the default grid and
      display the newly sized grid

--- script.js
+++ script.js
@@ -1,16 +1,62 @@
+let gridSize;
+
+// Ask user for grid size
+const gridSizeBtn = document.createElement("button");
+gridSizeBtn.textContent = `Resize`;
+document.body.appendChild(gridSizeBtn);
+
+// Create containerdiv element for grid
 const container = document.createElement("div");
 container.classList.add("container");
 document.body.appendChild(container);
 
-// Loop for creating a grid
-for (let i = 0; i < 256; i++) {
-  // Creates new div element
-  const newDiv = document.createElement("div");
-  // Adds the grid and grid-box class names to it
-  newDiv.classList.add(`grid`);
-  // Alters the size of the border
-  newDiv.style.height = "40px";
-  newDiv.style.width = "40px";
-  // appends it to the container div
-  container.appendChild(newDiv);
+// Initial grid
+createGrid(16);
+
+gridSizeBtn.addEventListener("click", (e) => {
+  gridSize = prompt("Please Enter a grid size");
+  // Remove Current grid
+  const grid = document.querySelectorAll(".grid");
+  grid.forEach((element) => {
+    element.remove();
+  });
+  // Create new grid
+  createGrid(gridSize);
+});
+
+// Change colour of element when hovering over it
+
+container.addEventListener("mouseover", (e) => {
+  console.log(e.target.className);
+  if (e.target.className === "grid") {
+    gridColour(e.target);
+  }
+});
+
+// Generates a random number
+function randomColour() {
+  return Math.floor(Math.random() * 255);
+}
+
+// Changes background color of element
+
+function gridColour(element) {
+  element.style.backgroundColor = `rgb(${randomColour()}, ${randomColour()}, ${randomColour()})`;
+}
+
+// Creates a grid with size inputted gridSize x gridSize
+function createGrid(gridSize) {
+  // Loop for creating a grid
+  for (let i = 0; i < gridSize ** 2; i++) {
+    // Creates new div element
+    const newDiv = document.createElement("div");
+    // Adds the grid and grid-box class names to it
+    newDiv.classList.add(`grid`);
+    // Alters the size of the box
+    const boxSize = 640 / gridSize;
+    newDiv.style.height = `${boxSize}px`;
+    newDiv.style.width = `${boxSize}px`;
+    // appends it to the container div
+    container.appendChild(newDiv);
+  }
 }
